28d47985414f4bf64bb981bfb91beccdc39e87fe
shadab97 <syedshadab97@rediffmail.com>
2020-03-09 23:39:43 +0530

    solved string task

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Div 2 A problems that i solved when i started codeforces
 
 ## 1 [watermelon](http://codeforces.com/contest/4/problem/A)
 ## 2 [Way Too Long Words](http://codeforces.com/contest/71/problem/A)
+## 3 [String Task](http://codeforces.com/problemset/problem/118/A)
---
+++ String Task.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool isvovel(char c){
+	if(c=='Y'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U'||c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='y'){
+		return true;
+	}else{
+		return false;
+	}
+}
+int main(){
+		string s;
+		cin>>s;
+		int l=s.length();
+		for(int i=0;i<l;i++){
+			if(isvovel(s[i])){
+				continue;
+			}
+			if(s[i]>='A' && s[i]<='Z'){
+				s[i]=s[i]+32;
+			}
+			cout<<"."<<s[i];
+		}
+	return 0;
+}
