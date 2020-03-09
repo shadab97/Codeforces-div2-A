e9476223573867ccefd945075a7b9edbd6c73db4
shadab97 <syedshadab97@rediffmail.com>
2020-03-09 22:38:42 +0530

    solved watermelon

---
+++ 1.Watermelon.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int i;
+	cin>>i;
+	if(i==2){
+		cout<<"NO";
+	}
+	else if(i%2==0){
+		cout<<"YES";
+		
+	}else{
+		cout<<"NO";
+	}
+	return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Codeforces-div2-A
 Div 2 A problems that i solved when i started codeforces
+
+#1 [watermelon](http://codeforces.com/contest/4/problem/A)
