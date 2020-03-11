7af55bbdde4b488c3863fad5a89c91776943e68f
shadab97 <syedshadab97@rediffmail.com>
2020-03-11 19:35:26 +0530

    solved Arrival of the general and numberse all question for easy access

---
+++ 11.Arrival of the General.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	int a[n];
+	int min=100;
+	int max=1;
+	int ii=0;
+	int jj=0;
+	for(int i=0;i<n;i++){
+		cin>>a[i];
+		if(a[i]>max){
+			max=a[i];
+			jj=i;
+		}
+		if(a[i]<=min){
+		min=a[i];
+		ii=i;	
+		}
+	}
+	int val=n-ii;
+	if(ii<jj){
+		
+		val+=jj-2;	
+	}
+	else{
+		val+=jj-1;
+	}
+	cout<<val;
+	return 0;
+}
